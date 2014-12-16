9d0eca93e1e9ee3942509b7550880dd4fad00d44
dukai <dukai86@gmail.com>
2014-12-16 13:21:34 +0800

    add js component

---
+++ resource/js/resource-loader.js
@@ -0,0 +1,118 @@
+define('resource-loader', function(require, exports, module){
+	var tools = require('./tools');
+	//资源载入器
+	var resourceLoader = {
+		totalCount: 0,
+		loadedCount: 0,
+		resources: {},
+		get: function(name){
+			if(this.resources[name] !== undefined){
+				return this.resources[name];
+			}else{
+				console.log('Error on get resource: ' + name);
+				return false;
+			}
+		},
+		load: function(resources){
+			this.totalCount = resources.length;
+			var self = this;
+			for(var i = 0, len = resources.length; i < len; i++){
+				switch(resources[i].type){
+					case 'image':{
+						this.loadImage(resources[i]);
+						break;
+					}
+	
+					case 'tmx':{
+						this.loadXML(resources[i]);
+						break;
+					}
+	
+					case 'audio':{
+						break;
+					}
+					
+					case 'json': {
+						this.loadJSON(resources[i]);
+						break;
+					}
+					default:
+						break;
+				}
+			};
+		},
+	
+		loadImage: function(resource){
+			var self = this;
+			var img = new Image();
+			img.dataName =resource.name;
+			img.onload = function(){
+				++self.loadedCount;
+				self.onProgress({loadedCount: self.loadedCount, totalCount: self.totalCount});
+				self.resources[this.dataName] = this;
+				if(self.totalCount === self.loadedCount){
+					self.onComplete();
+				}
+			};
+			
+			img.onerror = function(){
+				console.log('Error on: ' + this.dataName);
+			};
+			
+			img.src = resource.src;
+		},
+		
+		loadXML: function(resource){
+			var self = this;
+			
+			$.ajax({
+				url: resource.src,
+				dataType: 'xml',
+				success: function(rep, statusText, xhr){
+					++self.loadedCount;
+					self.resources[resource.name] = rep;
+					self.onProgress({loadedCount: self.loadedCount, totalCount: self.totalCount});
+	
+					if(self.totalCount === self.loadedCount){
+						self.onComplete();
+					}
+				},
+				error: function(statusText){
+					console.log('Error on: ' + statusText);
+				}
+			});
+	
+		},
+		
+		loadJSON: function(resource){
+			var self = this;
+			
+			$.ajax({
+				url: resource.src,
+				dataType: 'json',
+				success: function(rep, statusText, xhr){
+					++self.loadedCount;
+					self.resources[resource.name] = rep;
+					self.onProgress({loadedCount: self.loadedCount, totalCount: self.totalCount});
+	
+					if(self.totalCount === self.loadedCount){
+						self.onComplete();
+					}
+				},
+				error: function(statusText){
+					console.log('Error on: ' + statusText);
+				}
+			});
+		},
+	
+		loadAudio: function(){
+	
+		},
+	
+		onProgress: function(event){},
+		
+		onComplete: function(){}
+	};
+	
+	module.exports = resourceLoader;
+});
