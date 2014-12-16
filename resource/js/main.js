7c0c7b77bdab41bc92e87670e50d0ce14375e507
unclefreak <unclefreak@qq.com>
2014-12-16 16:39:06 +0800

    add loading animation demo

---
+++ resource/js/main.js
@@ -0,0 +1,33 @@
+define(function(require){
+	var RLoader = require('./resource-loader');
+	var loader = new RLoader();
+	loader.load([
+		{src: 'resource/images/background.jpg'},
+		{src: 'resource/images/load_1.png'},
+		{src: 'resource/images/load_2.png'},
+		{src: 'resource/images/load_3.png'},
+		{src: 'resource/images/load_4.png'},
+		{src: 'resource/images/load_5.png'},
+		{src: 'resource/images/load_6.png'},
+		{src: 'resource/images/load_7.png'},
+		{src: 'resource/images/load_8.png'},
+		{src: 'resource/images/load_9.png'},
+		{src: 'resource/images/title.png'}
+	]);
+	loader.on('progress', function(e){
+		if(e.loadedCount < 10){
+			$('section.load .imgbox img').attr('src', 'resource/images/load_' + e.loadedCount + ".png");
+		}
+	});
+	loader.on('load', function(e){
+		console.log(e);
+	});
+
+	var count = 1;
+	var timer = setInterval(function(){
+		$('section.load .imgbox img').attr('src', 'resource/images/load_' + (count++) + ".png");
+		if(count > 9){
+			clearInterval(timer);
+		}
+	}, 200);
+});
